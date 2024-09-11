4a375ce7c8b693eb158119ba293bc039a087de09
houlu <houlu8674@bupt.edu.cn>
2024-09-11 15:12:12 +0800

    change name

--- src/gxfw/App.vue
+++ src/gxfw/App.vue
@@ -14,7 +14,7 @@ import { mapActions } from 'vuex'
 // import { getGuid } from '@/utils/common.js'
 // import Cookies from 'js-cookie'
 export default {
-  name: 'Web',
+  name: 'Web233',
   data () {
     return {
       myListenerDate: {
